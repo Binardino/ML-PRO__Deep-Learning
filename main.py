0d885cd14de3808ad171f522c478ab9beb269a88
Binardino <langlois.robin@gmail.com>
2026-07-02 17:04:16 +0200

    feat: update main.py to use animal image dataset

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+from utilities import load_data
 from perceptron.dataset import generate_dataset, plot_decision_boundary
-from perceptron.neuron import artificial_neuron, predict
+from perceptron.neuron import artificial_neuron, predict, flatten_data
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+#update main for animal image dataset
 def main():
-    X, y = generate_dataset()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    W, b, loss_history = artificial_neuron(X_train, y_train, learning_rate=0.1, n_iter=10000)
+# used for percepton model
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = load_data()
+    X_train_reshape, X_test_reshape = flatten_data(X_train, X_test)
+    W, b, loss_history = artificial_neuron(X_train_reshape, y_train, learning_rate=0.1, n_iter=10000)
     print("\nFinal parameters:")
     print("W:", W)
     print("b:", b)
+    print("Final loss:", loss_history[-1])
 
     #accuracy
-    y_pred   = predict(X_test, W, b)
+    y_pred   = predict(X_test_reshape, W, b)
     accuracy = accuracy_score(y_test, y_pred)
     print(f"accuracy score : {accuracy * 100:.2f}%")
 
-    # Frontière de décision
-    plot_decision_boundary(X, y, W, b)
+    # Decision boundary - used for percepton model
+    #plot_decision_boundary(X_train, y_train, W, b)
 
 if __name__ == "__main__":
     main()
