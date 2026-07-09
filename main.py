2040baabccf3540bd8d3c6bc144596398ff57d5d
Binardino <langlois.robin@gmail.com>
2026-07-09 11:31:44 +0200

    feat: add normalisation step and fix load_data return order in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utilities import load_data
 from perceptron.dataset import generate_dataset, plot_decision_boundary
-from perceptron.neuron import artificial_neuron, predict, flatten_data
+from perceptron.neuron import artificial_neuron, predict, flatten_data, normalise_data
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
@@ -8,16 +8,19 @@ from sklearn.model_selection import train_test_split
 def main():
 # used for percepton model
 #     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    X_train, X_test, y_train, y_test = load_data()
+    X_train, y_train, X_test, y_test = load_data()
+    #flattening data
     X_train_reshape, X_test_reshape = flatten_data(X_train, X_test)
-    W, b, loss_history = artificial_neuron(X_train_reshape, y_train, learning_rate=0.1, n_iter=10000)
+    # MinMax normalisation
+    X_train_norm, X_test_norm = normalise_data(X_train_reshape, X_test_reshape)
+    W, b, loss_history = artificial_neuron(X_train_norm, y_train, learning_rate=0.1, n_iter=1000)
     print("\nFinal parameters:")
     print("W:", W)
     print("b:", b)
     print("Final loss:", loss_history[-1])
 
     #accuracy
-    y_pred   = predict(X_test_reshape, W, b)
+    y_pred   = predict(X_test_norm, W, b)
     accuracy = accuracy_score(y_test, y_pred)
     print(f"accuracy score : {accuracy * 100:.2f}%")
 
